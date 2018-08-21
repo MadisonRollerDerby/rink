15b03b7910c577f3fc68582c02448c723c6e01a8
Dan Silvers <dan@silvers.net>
2018-08-20 22:24:40 -0500

    Remove celery testing settings

--- rink/taskapp/celery.py
+++ rink/taskapp/celery.py
@@ -11,18 +11,18 @@ if not settings.configured:
 app = Celery(
     'rink',
 
-    task_always_eager=True,
+    task_always_eager=False,
     task_time_limit=10,
-    #redis_socket_connect_timeout=10,
-    #redis_socket_timeout=8,
+    redis_socket_connect_timeout=10,
+    redis_socket_timeout=8,
     broker_connection_timeout=9,
-    broker="amqp://guest:guest@localhost:5672/test_rink",
-    backend="amqp://guest:guest@localhost:5672/test_rink",
+    #broker="amqp://guest:guest@localhost:5672/test_rink",
+    #backend="amqp://guest:guest@localhost:5672/test_rink",
 
-    #broker="redis://",
-    #broker_transport="redis",
-    #broker_url='redis://localhost:6379',
-    #result_backend='redis',  #://localhost:6379',
+    broker="redis://",
+    broker_transport="redis",
+    broker_url='redis://localhost:6379',
+    result_backend='redis',  #://localhost:6379',
 
     #accept_content=['json'],
     #task_serializer='json',
