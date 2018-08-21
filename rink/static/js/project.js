1ecf8ddddc60076f5084fd4cb683d8cbe853e792
Dan Silvers <dan@silvers.net>
2018-08-20 22:33:53 -0500

    Fix CSRF errors on dynamic forms

--- rink/static/js/project.js
+++ rink/static/js/project.js
@@ -43,7 +43,6 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-var csrftoken = getCookie('csrftoken');
 
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
