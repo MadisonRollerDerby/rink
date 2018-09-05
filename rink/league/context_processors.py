89d30c56edb5128877ba191314658d9ea7e8fc9f
Dan Silvers <dan@silvers.net>
2018-09-05 11:59:00 -0500

    Fix cross-league signup form theming issues #37

--- rink/league/context_processors.py
+++ rink/league/context_processors.py
@@ -1,16 +1,33 @@
 from django.conf import settings
 
 from league.models import League
+from registration.models import RegistrationEvent
 
 
 def league_processor(request):
     league = None
-    league_id = request.session.get('view_league', None)
-    if league_id:
-        try:
-            league = League.objects.get(pk=league_id)
-        except League.DoesNotExist:
-            pass
+
+    # Override the theme for registration forms. Sometimes a user has a different
+    # league set after login compared to the one attached to a registration event.
+    try:
+        if 'register' in request.resolver_match.namespaces:
+            search_kwargs = {
+                'league__slug': request.resolver_match.kwargs.get('league_slug', None),
+                'slug': request.resolver_match.kwargs.get('event_slug', None),
+            }
+            league = RegistrationEvent.objects.get(**search_kwargs).league
+    except AttributeError:
+        pass
+    except RegistrationEvent.DoesNotExist:
+        pass
+
+    if not league:
+        league_id = request.session.get('view_league', None)
+        if league_id:
+            try:
+                league = League.objects.get(pk=league_id)
+            except League.DoesNotExist:
+                pass
 
     return {'league_template': league, }
 
